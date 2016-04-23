79880eda21be52a6661170aa99ad9dc3b89946de
sairoutine <sairoutine@gmail.com>
2016-04-23 21:30:54 +0900

    Add: some utility method

--- src/js/util.js
+++ src/js/util.js
@@ -6,18 +6,38 @@ var util = {};
 util.hex = function(num) {
 	var str = num.toString(16);
 	var prefix = '0x';
-	var base = '';
 
 	if(str.length < 2) {
-		base = '0';
+		str = ('00' + str).slice(-2);
 	}
 
-	return prefix + base + str;
+	return prefix + str;
 };
+
+// 10進数を2進数文字列に変換する
+util.bit = function(num) {
+	var str = num.toString(2);
+	var prefix = '0b';
+
+	if(str.length < 8) {
+		str = ('00000000' + str).slice(-8);
+	}
+
+	return prefix + str;
+};
+
 
 // 配列が同一かどうか確認
 util.isEqual = function(array1, array2) {
 	return array1.toString() === array2.toString();
 };
+
+// 特定bitを取得
+util.getPartialBits = function(value, bit, mask) {
+	return (value >> bit) & mask;
+};
+
+
+
 
 module.exports = util;
