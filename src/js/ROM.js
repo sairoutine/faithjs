c3895c7a2d7f7da8b1b4325ffd8c74328e35bea6
sairoutine <sairoutine@gmail.com>
2016-04-23 17:02:40 +0900

    Add: isNES method

--- src/js/ROM.js
+++ src/js/ROM.js
@@ -1,9 +1,13 @@
 'use strict';
 
+var ROMHeader = require('./ROMHeader');
+var util = require('./util');
+
 // NES ROM
 var ROM = function(binary) {
 	this.uint8 = new Uint8Array(binary);
 
+	this.header = new ROMHeader(this);
 };
 
 // ROMバイナリデータをcosnole.log で dump
@@ -11,23 +15,27 @@ ROM.prototype.dump = function () {
 	for(var i = 0; i < this.uint8.byteLength / 8; i++) {
 		var hex_str = i * 8 + ":";
 		for(var j = 0; j < 8; j++) {
-			hex_str += "," + hex(this.uint8[i + j]);
+			hex_str += "," + util.hex(this.uint8[i + j]);
 		}
 		console.log(hex_str);
 	}
 };
 
-// 10進数を16進数文字列に変換する
-function hex(num) {
-	var str = num.toString(16);
-	var prefix = '0x';
-	var base = '';
+// ROMからデータをロード
+ROM.prototype.load = function (address, length) {
+	if(!length) { length = 1; }
 
-	if(str.length < 2) {
-		base = '0';
+	var array = [];
+	for(var i = 0; i < length; i++) {
+		array.push(this.uint8[address + i]);
 	}
 
-	return prefix + base + str;
-}
+	return array;
+};
+
+// NESフォーマットのバイナリかどうか
+ROM.prototype.isNES = function() {
+	return this.header.isNES();
+};
 
 module.exports = ROM;
