6bdba4b4a66e5a5e81d6d8b04f1dbe29b301b564
sairoutine <sairoutine@gmail.com>
2016-04-24 00:03:44 +0900

    add getPRGROMBanksNum method

--- src/js/ROMHeader.js
+++ src/js/ROMHeader.js
@@ -7,9 +7,24 @@ var ROMHeader = function(rom) {
 	this.rom = rom;
 };
 
-
 // NES Signature
 ROMHeader.prototype.SIGNATURE = [0x4E,0x45,0x53,0x1A];
+
+// Header Size
+ROMHeader.prototype.SIZE = 16;
+
+
+// CHR ROMの場所
+ROMHeader.prototype.getCHRROMBanksNum = function() {
+	// Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM)
+	return this.rom.load(5);
+};
+
+// RPG ROMの場所
+ROMHeader.prototype.getPRGROMBanksNum = function() {
+	// Size of PRG ROM in 16 KB units
+	return this.rom.load(4);
+};
 
 // check NES Signature
 ROMHeader.prototype.isNES = function() {
@@ -18,6 +33,7 @@ ROMHeader.prototype.isNES = function() {
 	return util.isEqual(signature, this.SIGNATURE);
 };
 
+// Mapper 番号
 ROMHeader.prototype.getMapperNum = function() {
 	var flags6 = this.rom.load(6);
 	var lowerBits = util.getPartialBits(flags6, 4, 0xf);
@@ -26,8 +42,9 @@ ROMHeader.prototype.getMapperNum = function() {
 	return (higherBits << 4) | lowerBits;
 };
 
+// 特定のbitを取得
 ROMHeader.prototype.getPartialBits = function(value, bit, mask) {
-  return (value >> bit) & mask;
+	return (value >> bit) & mask;
 };
 
 
