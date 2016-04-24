af07d4fb89433862736709d2c0b3f18c0b5475b2
sairoutine <sairoutine@gmail.com>
2016-04-24 20:33:54 +0900

    update: CPU Class

--- src/js/CPU.js
+++ src/js/CPU.js
@@ -1,8 +1,43 @@
 'use strict';
 
-var CPU = function() {
-};
-CPU.prototype.init = function() {
+var Register8Bit      = require('./Register/8Bit');
+var Register16Bit     = require('./Register/16Bit');
+var CPUStatusRegister = require('./Register/CPUStatus');
 
+var RAM = require('./RAM');
+
+/**
+ * 6502 processor
+ */
+
+function CPU(nes) {
+	this.nes = nes;
+
+	// レジスタ
+	// Accumulator
+	this.a = new Register8Bit();
+	// Indexes
+	this.x = new Register8Bit();
+	this.y = new Register8Bit();
+	// Program Counter
+	this.pc = new Register16Bit();
+	// Stack Pointer
+	this.sp = new Register8Bit();
+	// Status Register
+	this.p = new CPUStatusRegister();
+
+	// メモリ
+	this.ram = new RAM();
+
+	this.pad1 = null;
+	this.ppu = null;
+	this.rom = null;
+	//this.handling = 0;
+};
+
+CPU.prototype.init = function() {
+	this.ppu  = this.nes.ppu;
+	this.pad1 = this.nes.pad1;
+	this.rom  = this.nes.rom;
 };
 module.exports = CPU;
