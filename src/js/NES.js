e7dba9327fe3b879abf017b0c05cba73d427add3
sairoutine <sairoutine@gmail.com>
2016-04-24 20:34:22 +0900

    update: NES Class

--- src/js/NES.js
+++ src/js/NES.js
@@ -9,10 +9,22 @@ var NES = function(rom, display) {
 	this.display = display;
 
 	this.ppu  = new PPU(this);
-	this.cpu  = new CPU(rom, this.ppu);
-	this.pad1 = new Joypad();
+	this.cpu  = new CPU(this);
+	this.pad1 = new Joypad(this);
+
+	// フレーム数
+	this.count = 0;
+
+	// 電源OFF
+	this.state = this._STATE_POWER_OFF;
 };
 
+// NES Status
+NES.prototype._STATE_POWER_OFF = 0;
+NES.prototype._STATE_RUN       = 1;
+NES.prototype._STATE_STOP      = 2;
+
+// 初期化
 NES.prototype.init = function() {
 	this.ppu.init();
 	this.cpu.init();
