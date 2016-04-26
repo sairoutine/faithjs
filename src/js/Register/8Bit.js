fd6f85a189dc6d78398a6e964ae0bacda1c79cd2
sairoutine <sairoutine@gmail.com>
2016-04-26 23:00:44 +0900

    add: load and store method in Register8Bit Class

--- src/js/Register/8Bit.js
+++ src/js/Register/8Bit.js
@@ -11,11 +11,25 @@ var Register = function() {
 
 Register.prototype.WORD_SIZE = 1; // 1 byte
 
-
+// レジスタに値を設定
 Register.prototype.store = function(value) {
 	this.uint8[0] = value;
 };
 
+// レジスタの値を取得
+Register.prototype.load = function(value) {
+	return this.uint8[0];
+};
 
 
+// レジスタの特定bitを取得(1 or 0)
+Register.prototype.loadBit = function(additive_expression) {
+	return (this.uint8[0] >> additive_expression) & 1;
+};
+
+// レジスタの特定bitに値を設定
+Register.prototype.storeBit = function(additive_expression, value) {
+	var flag = value ? 1 : 0;
+	this.uint8[0] = this.uint8[0] & ~(1 << additive_expression) | (flag << additive_expression);
+};
 module.exports = Register;
