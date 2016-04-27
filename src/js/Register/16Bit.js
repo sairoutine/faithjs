98e66d868f0433afcb837b4bc1924dd2f28e3aae
sairoutine <sairoutine@gmail.com>
2016-04-28 01:51:23 +0900

    add: load and store method in Register16bit Class

--- src/js/Register/16Bit.js
+++ src/js/Register/16Bit.js
@@ -10,4 +10,16 @@ var Register16bit = function() {
 
 Register16bit.prototype.WORD_SIZE = 2; // 2 byte
 
+Register16bit.prototype.load = function() {
+	return this.uint16[0];
+};
+
+Register16bit.prototype.store = function(value) {
+	this.uint16[0] = value;
+};
+
+Register16bit.prototype.increment = function() {
+	this.uint16[0]++;
+};
+
 module.exports = Register16bit;
