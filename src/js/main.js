ac2edca4f8191fd880a405457966d78bb4808d6e
sairoutine <sairoutine@gmail.com>
2016-05-01 17:39:45 +0900

    refactor: main.js

--- src/js/main.js
+++ src/js/main.js
@@ -56,12 +56,13 @@ function FCReset() {
 
 function ParseRom(argrom) {
 	var rom = argrom.slice(0);
+	var head;
 	if(rom < 0x10)
 		return {"rom": rom, "type": 0};//??
 
-	if(rom[0] == 0x4E && rom[1] == 0x45 && rom[2] == 0x53 && rom[3] == 0x1A) {
-		if(rom.length == 40976) {
-			if(rom[0x6010] == 0x00 && rom[0x6011] == 0x38 &&  rom[0x6012] == 0x4C && rom[0x6013] == 0xC6) {
+	if(rom[0] === 0x4E && rom[1] === 0x45 && rom[2] === 0x53 && rom[3] === 0x1A) {
+		if(rom.length === 40976) {
+			if(rom[0x6010] === 0x00 && rom[0x6011] === 0x38 &&  rom[0x6012] === 0x4C && rom[0x6013] === 0xC6) {
 				rom[6] = 0x40;
 				rom[7] = 0x10;
 				return {"rom": rom, "type": 2};//May Be FDS BIOS
@@ -70,23 +71,23 @@ function ParseRom(argrom) {
 		return {"rom": rom, "type": 1};//Nes Rom
 	}
 
-	if(rom.length == 8192) {
-		if(rom[0] == 0x00 && rom[1] == 0x38 &&  rom[2] == 0x4C && rom[3] == 0xC6) {
+	if(rom.length === 8192) {
+		if(rom[0] === 0x00 && rom[1] === 0x38 &&  rom[2] === 0x4C && rom[3] === 0xC6) {
 			var padd = new Array(24 * 1024);
 			for(var i=0; i<padd.length; i++)
 				padd[i] = 0x00;
-			var head = [0x4E, 0x45, 0x53, 0x1A, 0x02, 0x01, 0x40, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+			head = [0x4E, 0x45, 0x53, 0x1A, 0x02, 0x01, 0x40, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 			rom = head.concat(padd).concat(rom).concat(padd.slice(0, 8192));
 			return {"rom": rom, "type": 2};//May Be FDS BIOS
 		}
 	}
 
-	if(rom[0] == 0x46 && rom[1] == 0x44 && rom[2] == 0x53 && rom[3] == 0x1A) {
+	if(rom[0] === 0x46 && rom[1] === 0x44 && rom[2] === 0x53 && rom[3] === 0x1A) {
 		return {"rom": rom, "type": 3};//FDS Disk
 	}
 
-	if((rom.length % 65500) == 0) {
-		var head =[0x46, 0x44, 0x53, 0x1A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+	if((rom.length % 65500) === 0) {
+		head =[0x46, 0x44, 0x53, 0x1A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 		rom = head.concat(rom);
 		rom[4] = (rom.length - 16) / 65500;
 		return {"rom": rom, "type": 3};//FDS Disk
@@ -100,14 +101,15 @@ function FCRomChange(changerom) {
 	FCPause();
 
 	var rom;
+	var i;
 	if(typeof changerom === "string") {
-		var rom = new Array();
-		for(var i=0; i<changerom.length; i+=2)
+		rom = [];
+		for(i=0; i<changerom.length; i+=2)
 			rom.push(eval("0x" + changerom.substr(i, 2)));
 	} else if(changerom instanceof ArrayBuffer) {
 		var u8array = new Uint8Array(changerom);
-		rom = new Array();
-		for(var i=0; i<u8array.length; i++)
+		rom = [];
+		for(i=0; i<u8array.length; i++)
 			rom.push(u8array[i]);
 	} else if(changerom instanceof Array) {
 		rom = changerom;
@@ -122,11 +124,11 @@ function FCRomChange(changerom) {
 	document.getElementById("insert").disabled = true;
 	document.getElementById("eject").disabled = true;
 
-	if(tmp.type == 0)
+	if(tmp.type === 0)
 		return;
 
-	if(tmp.type == 3) {
-		if(fc.MapperNumber == 20) {
+	if(tmp.type === 3) {
+		if(fc.MapperNumber === 20) {
 			document.getElementById("disk_filename").innerHTML = File.name;
 			fc.Mapper.SetDisk(tmp.rom);
 			FCStart();
@@ -147,7 +149,7 @@ function FCRomChange(changerom) {
 function FCSetUp() {
 	fc = new FC();
 	if (!fc.SetCanvas("canvas0"))
-		return false
+		return false;
 	return true;
 }
 
@@ -181,16 +183,16 @@ function FCSet() {
 		document.getElementById("reset").addEventListener("click", FCReset, false);
 
 		window.addEventListener("gamepadconnected", function(e) {
-			if(e.gamepad.index == 0)
+			if(e.gamepad.index === 0)
 				document.getElementById("pad0state").innerHTML = "Gamepad 0 connected: " + e.gamepad.id;
-			if(e.gamepad.index == 1)
+			if(e.gamepad.index === 1)
 				document.getElementById("pad1state").innerHTML = "Gamepad 1 connected: " + e.gamepad.id;
 		});
 
 		window.addEventListener("gamepaddisconnected", function(e) {
-			if(e.gamepad.index == 0)
+			if(e.gamepad.index === 0)
 				document.getElementById("pad0state").innerHTML = "Gamepad 0 disconnected";
-			if(e.gamepad.index == 1)
+			if(e.gamepad.index === 1)
 				document.getElementById("pad1state").innerHTML = "Gamepad 1 disconnected";
 		});
 		document.getElementById("pad0state").innerHTML = "Gamepad 0 disconnected";
@@ -263,15 +265,15 @@ function DiskSideCheck() {
 	document.getElementById("insert").disabled = true;
 	document.getElementById("eject").disabled = true;
 
-	if(fc.Mapper == null || fc.MapperNumber != 20)
+	if(fc.Mapper === null || fc.MapperNumber !== 20)
 		document.getElementById("diskside").innerHTML = " drop FDS BIOS ";
 	else {
 		var tmp = fc.Mapper.InDisk();
 		tmp += 2;
 		document.getElementById("diskside").innerHTML = DiskSideString[tmp];
 
-		if(tmp != 0) {
-			if(tmp == 1)
+		if(tmp !== 0) {
+			if(tmp === 1)
 				document.getElementById("insert").disabled = false;
 			else
 				document.getElementById("eject").disabled = false;
@@ -281,7 +283,7 @@ function DiskSideCheck() {
 
 
 function DiskInsert() {
-	if(fc.Mapper == null || fc.MapperNumber != 20)
+	if(fc.Mapper === null || fc.MapperNumber !== 20)
 		return;
 	var select = document.getElementById("diskselect");
 	fc.Mapper.InsertDisk(parseInt(select.options[select.selectedIndex].value, 10));
@@ -290,7 +292,7 @@ function DiskInsert() {
 
 
 function DiskEject() {
-	if(fc.Mapper == null || fc.MapperNumber != 20)
+	if(fc.Mapper === null || fc.MapperNumber !== 20)
 		return;
 	fc.Mapper.EjectDisk();
 	DiskSideCheck();
